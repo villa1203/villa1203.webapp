1034f595f329ae32d9b10af35b04eb0ed2729473
azertypow <azertypow@gmail.com>
2022-07-14 13:36:14 +0200

    init

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="v-layout">
+    <Content />
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue"
+
+export default defineComponent({
+
+})</script>
+
+<style lang="scss">
+.v-layout {
+}
+</style>
