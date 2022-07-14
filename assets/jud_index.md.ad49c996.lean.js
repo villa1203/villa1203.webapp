0912dad637e9934028c9cafa56d2b284993aa6cb
azertypow <azertypow@users.noreply.github.com>
2022-07-14 14:59:56 +0000

    deploy: 13b935135534065c30d7aac61c07a94993411c0e

---
+++ assets/jud_index.md.ad49c996.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as n}from"./app.41223962.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"jud/index.md"}'),o={name:"jud/index.md"},s=n("p",null,"jud",-1),r=[s];function d(c,_,i,p,l,u){return a(),t("div",null,r)}var x=e(o,[["render",d]]);export{f as __pageData,x as default};
