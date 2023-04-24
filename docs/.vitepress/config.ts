5687edd9fc224796c301306484e1f0c2167e6dbb
azertypow <azertypow@gmail.com>
2023-04-24 12:41:30 +0200

    feat: update link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   themeConfig: {
     nav: [
-      { text: 'nico', link: '/nico/' },
+      { text: 'anne',  link: '/anne/' },
       { text: 'jud',  link: '/jud/' },
+      { text: 'nico', link: '/nico/' },
+      { text: 'ressources', link: '/ressources/' },
     ]
   }
 })
