5b79f7b23fe84a67f7e633467ebc201e1007b06f
azertypow <azertypow@users.noreply.github.com>
2023-04-24 10:37:29 +0000

    deploy: 1f28e969ee5b869639f4f54cc69b9130e8c75775

---
+++ assets/anne_index.md.32beed26.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as n}from"./app.774d3e76.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"anne/index.md"}'),o={name:"anne/index.md"},s=n("p",null,"Hello",-1),r=[s];function c(d,_,i,l,p,m){return a(),t("div",null,r)}var x=e(o,[["render",c]]);export{u as __pageData,x as default};
