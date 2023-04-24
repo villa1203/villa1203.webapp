ea1d7510a9b277889257b5a166cfcee94bb289f8
azertypow <azertypow@gmail.com>
2023-04-24 14:03:37 +0200

    fix: title

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   themeConfig: {
+    siteTitle: '🏡 villa1203',
     nav: [
       { text: 'anne',  link: '/anne/' },
       { text: 'jud',  link: '/jud/' },
